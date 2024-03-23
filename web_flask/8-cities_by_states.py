3aa21c8058ee1f58edb057606339a1249c048b39
Jeff Kirui <64260549+JeffKirui@users.noreply.github.com>
2024-03-23 14:19:10 +0300

    Task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,37 +1,30 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
+""" This Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
-
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """reload close files storage"""
+def closedb(exc):
+    """This is to close a database session"""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states')
 def states_list():
-    """display a HTML page: (inside the tag BODY)"""
-    context = storage.all(State).values()
-    return render_template('7-states_list.html', states=context)
-
-
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """present in DBStorage sorted by name (A->Z)"""
-    states_complete = {
-        'states': storage.all(State).values(),
-        'cities': storage.all(City).values()
-    }
-
-    return render_template('8-cities_by_states.html', **states_complete)
+    """ /states_list route """
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    storage.reload()
+    app.run("0.0.0.0", 5000)
