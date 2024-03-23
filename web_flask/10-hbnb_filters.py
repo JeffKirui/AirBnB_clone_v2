f2f144cbbecad0fa7b17243118ba5666814687d0
Jeff Kirui <64260549+JeffKirui@users.noreply.github.com>
2024-03-23 14:30:10 +0300

    Task 11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,46 +1,29 @@
 #!/usr/bin/python3
 """
-cities by state
+This Starts a Flask web application. Listens on 0.0.0.0  on port 5000.
+Routes:    * /hbnb_filters: HBnB HTML filters page.
 """
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from os import getenv
 
 app = Flask(__name__)
 
 
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """Displays the HBnB filters HTML page."""
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
+
+
 @app.teardown_appcontext
-def teardown(self):
-    """ close files storage
-    """
+def teardown(excpt=None):
+    """Remove the current SQLAlchemy Session."""
     storage.close()
 
 
-@app.route('/hbnb_filters')
-def hbnb_filters():
-    """ create and return dictionary
-    """
-    conntext = {
-        'states': storage.all(State).values(),
-        'cities': storage.all(City).values(),
-        'amenities': storage.all(Amenity).values()
-    }
-    return render_template('10-hbnb_filters.html', **conntext)
-
-
-@app.route('/cities_by_states')
-def cities_list():
-    """" Display states and cities
-    """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        states = storage.all(State).values()
-    else:
-        states = storage.all(State).values()
-    return render_template('8-cities_by_states.html', states=states)
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
